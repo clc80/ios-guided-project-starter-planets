2856e12305d4d36dc801af0bf75c94a1b2c10469
Claudia Maciel <clc_80@yahoo.com>
2020-02-19 19:15:52 -0800

    IBOutlets and IBActions Set

---
+++ Planets/View Controllers/SettingsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SettingsViewController.swift
+//  Planets
+//
+//  Created by Claudia Contreras on 2/19/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    @IBOutlet var plutoToggle: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    //MARK: - IBActions
+    @IBAction func didTogglePluto(_ sender: UISwitch) {
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Planets/Views/PlanetCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Claudia Contreras on 2/19/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - IBOutlets
+    @IBOutlet var planetImageView: UIImageView!
+    @IBOutlet var planetNameLabel: UILabel!
+    
+    
+    
+}
